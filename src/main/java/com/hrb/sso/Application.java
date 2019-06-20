3dd04cb305c8697d55f051c9da2d54e3e8ec2984
1172404214@qq.com <qzgang123>
2019-06-20 17:03:47 +0800

    springnoot+redis+zk+mybatis

--- src/main/java/com/hrb/sso/Application.java
+++ src/main/java/com/hrb/sso/Application.java
@@ -2,7 +2,6 @@ package com.hrb.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cloud.zookeeper.config.ZookeeperConfigAutoConfiguration;
 
 /**
