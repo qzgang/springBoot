2d172b42668e867c45684fe7b3a1a87455ce5573
1172404214@qq.com <qzgang123>
2019-06-20 17:07:32 +0800

    springnoot+redis+zk+mybatis

---
+++ src/test/java/com/hrb/sso/CodeGeneration.java
@@ -0,0 +1,82 @@
+package com.hrb.sso;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.io.File;
+import java.net.URLDecoder;
+
+/**
+ * @Description: 代码生成器
+ * @author qzg
+ */
+public class CodeGeneration {
+    /**
+     * @Title: main
+     * @Description: 生成
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+
+        String targetPath = Class.class.getClass().getResource("/").getPath();
+        String currrntPath = URLDecoder.decode(targetPath.substring(0, targetPath.length() - 20), "utf-8");
+        System.out.println(currrntPath);///E:/springBoot-workspace/hrb-sso/
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(currrntPath + "/src/main/java");//输出文件路径
+        gc.setFileOverride(true);
+        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setControllerName("%sController");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sDao");
+        gc.setXmlName("%sDao");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setUrl("jdbc:mysql://10.0.0.53/dev_hurp2p_zl?useUnicode=true&characterEncoding=utf8");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // strategy.setTablePrefix(new String[] { "sys_" });// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(new String[] { "rz_bus_record" }); // 需要生成的表
+        strategy.setSuperServiceClass(null);
+        strategy.setSuperServiceImplClass(null);
+        strategy.setSuperMapperClass(null);
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        String[] tables = strategy.getInclude();
+        String str = tables[0].toLowerCase().split("_")[0];
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.hrb.sso"+"."+str);
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("service.impl");
+        pc.setMapper("dao");
+        pc.setEntity("entity");
+        pc.setXml("dao");
+        mpg.setPackageInfo(pc);
+
+        // 执行生成
+        mpg.execute();
+    }
+}
