adaf1bb7fd7ab9b63a578e7618935cd765698703
1172404214@qq.com <qzgang123>
2019-06-20 10:25:21 +0800

    springnoot+redis+zk+mybatis

--- src/main/java/com/hrb/sso/Application.java
+++ src/main/java/com/hrb/sso/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrbSsoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(HrbSsoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
