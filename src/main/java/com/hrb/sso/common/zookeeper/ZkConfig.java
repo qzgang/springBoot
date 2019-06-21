1cf92eb827f191f12493ca4f7228b01eea6d525f
1172404214@qq.com <qzgang123>
2019-06-21 13:32:33 +0800

    springnoot+redis+zk+mybatis

--- src/main/java/com/hrb/sso/common/zookeeper/ZkConfig.java
+++ src/main/java/com/hrb/sso/common/zookeeper/ZkConfig.java
@@ -4,9 +4,15 @@ package com.hrb.sso.common.zookeeper;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "config")
 @Data
+@ConfigurationProperties(prefix = "hrb-test")
 public class ZkConfig {
-    public String username;
-    public String password;
+    public String oracle_jdbc_url;
+    public String oracle_jdbc_username;
+    public String oracle_jdbc_password;
+    public String mysql_jdbc_url;
+    public String mysql_jdbc_username;
+    public String mysql_jdbc_password;
+    public String redis_server;
+
 }
