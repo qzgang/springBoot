8394ba9fedaacd52ccf508d4fc487e4cf899ec8e
1172404214@qq.com <qzgang123>
2019-06-25 09:58:56 +0800

    springnoot+redis+zk+mybatis

--- src/main/java/com/hrb/sso/Application.java
+++ src/main/java/com/hrb/sso/Application.java
@@ -1,16 +1,13 @@
 package com.hrb.sso;
 
-import com.hrb.sso.common.zookeeper.config.ZkConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 /**
  * @author
  */
 @SpringBootApplication
-@EnableConfigurationProperties({ZkConfig.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
