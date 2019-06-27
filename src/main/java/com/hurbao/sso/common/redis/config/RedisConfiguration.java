7b3c7be2f059cdec062dd571099ac8c2f6301daa
1172404214@qq.com <qzgang123>
2019-06-27 20:07:46 +0800

    springnoot+redis+zk+mybatis

--- src/main/java/com/hurbao/sso/common/redis/config/RedisConfiguration.java
+++ src/main/java/com/hurbao/sso/common/redis/config/RedisConfiguration.java
@@ -1,12 +1,10 @@
 package com.hurbao.sso.common.redis.config;
 import com.google.common.collect.Sets;
-import com.hurbao.sso.common.zookeeper.config.ZkConfig;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.hurbao.sso.common.zookeeper.cache.ZkProjectCache;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 
 import java.util.Collections;
 import java.util.Set;
@@ -16,12 +14,9 @@ import java.util.Set;
  */
 @Configuration
 public class RedisConfiguration extends CachingConfigurerSupport {
-    @Autowired
-    ZkConfig zkConfig;
 
     @Bean(name="jedisCluster")
     public JedisClusterFactory jedisCluster(
-           @Value("${redis.cluster.redis-server}") String redis_servers,
            @Value("${redis.cluster.connection-timeout}") int connectionTimeout,
            @Value("${redis.cluster.so-timeout}")int soTimeout,
            @Value("${redis.cluster.max-redirections}") int maxRedirections) {
@@ -29,12 +24,7 @@ public class RedisConfiguration extends CachingConfigurerSupport {
         jedisClusterFactory.setConnectionTimeout(connectionTimeout);
         jedisClusterFactory.setSoTimeout(soTimeout);
         jedisClusterFactory.setMaxRedirections(maxRedirections);
-        String[] split = null;
-        if(StringUtils.isEmpty(zkConfig.getRedis_server())){
-            split = redis_servers.split(",");
-        }else{
-            split = zkConfig.getRedis_server().split(",");
-        }
+        String[] split = ZkProjectCache.getVal("redis.server").split(",");
         Set<String> hosts = Sets.newHashSet();
         Collections.addAll(hosts, split);
         jedisClusterFactory.setJedisClusterNodes(hosts);
